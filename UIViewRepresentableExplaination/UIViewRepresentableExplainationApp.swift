97df6e507161d223815b9ad0ca77cd05307e36eb
FlorianTo <florian.tousch@hotmail.fr>
2022-02-01 20:36:48 +0100

    Initial Commit

---
+++ UIViewRepresentableExplaination/UIViewRepresentableExplainationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewRepresentableExplainationApp.swift
+//  UIViewRepresentableExplaination
+//
+//  Created by Florian on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct UIViewRepresentableExplainationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
